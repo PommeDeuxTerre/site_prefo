17b0ef824f284c4fef5e136202c4bda3924d09c2
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2023-10-27 21:05:38 +0200

    pacman move in the good direction

--- js/maze.js
+++ js/maze.js
@@ -3,6 +3,9 @@ var moves = [top_wall,left_wall,right_wall,bottom_wall];
 var opposite_moves = [bottom_wall,right_wall,left_wall,top_wall];
 var get_move_dir = [-10,-1,1,10]
 var inverse_move = [3,2,1,0]
+var pacman_pos = 45;
+
+var cursor_pos = null;
 
 function set_grid()
 {
@@ -15,13 +18,15 @@ function set_grid()
         for (var j=0;j<10;j++)
         {
             const square = document.createElement("div")
-            square.addEventListener("click",function click(){top_wall(this);bottom_wall(this);left_wall(this);right_wall(this)})
+            square.addEventListener("mouseover",function click(){cursor_pos=this.id})
+            square.addEventListener("mouseout",function click(){cursor_pos=null})
             square.id = ""+(i*10+j)
             line_html.appendChild(square)
         }
         maze_html.appendChild(line_html)
     }
     document.body.appendChild(maze_html)
+    document.getElementById(pacman_pos).style.backgroundColor="yellow"
 }
 
 function get_random(possibilities_number)
@@ -125,7 +130,60 @@ function maze_generator(index)
     }
 }
 
+function get_distance(a,b)
+{
+    y = Math.round(a/10) - Math.round(b/10);
+    console.log(y)
+    x = a%10 - b%10;
+    console.log(x)
+    console.log("x: "+x+"y: "+y)
+    return Math.sqrt(x*x+y*y)
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function get_pacman_move(pac_pos)
+{
+    var best_move = 0;
+    var best_score = 100000;
+    var score;
+    for (var i=0;i<4;i++)
+    {
+        if (grid[pac_pos] & 2**i)
+        {
+            score = get_distance(cursor_pos, pac_pos+get_move_dir[i])
+            if (score<best_score)
+            {
+                best_score=score
+                best_move = i
+            }
+        }
+    }
+    return best_move;
+}
+
+async function pacman_move()
+{
+    var move;
+    while (true)
+    {
+        await sleep(1000)
+        console.log("curseur: "+cursor_pos)
+        console.log("pacman: "+pacman_pos)
+        if (cursor_pos!=null && cursor_pos!=pacman_pos)
+        {
+            document.getElementById(pacman_pos).style.backgroundColor="#8d99ae"
+            move = pacman_pos+get_move_dir[get_pacman_move(pacman_pos)]
+            document.getElementById(move).style.backgroundColor="yellow"
+            pacman_pos = move;
+        }
+    }
+}
+
 set_grid()
 var starter_index = get_random(100);
 grid[starter_index] = 0
 maze_generator(starter_index)
+pacman_move()
