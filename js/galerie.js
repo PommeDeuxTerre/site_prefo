b49439490a098db00de8056f23eddbae06e04930
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2023-11-05 10:19:25 +0100

    add counter

--- js/galerie.js
+++ js/galerie.js
@@ -1,18 +1,11 @@
-const nb_photos = 7;
+const nb_photos = 18;
 var photos_index = 0;
 
 function next_photos()
 {
     photos_index-=-3;
-    const photos = document.getElementById("photos");
-    photos.innerHTML=""
-    for (var i=0;i<3;i-=-1)
-    {
-        var photo = document.createElement("img");
-        photo.src="../img/"+(photos_index+i)%nb_photos+".jpg"
-        photo.className="photo_expo"
-        photos.appendChild(photo)
-    }
+    photos_index%=nb_photos
+    photos_actualise()
 }
 
 function previous_photos()
@@ -22,13 +15,24 @@ function previous_photos()
     {
         photos_index+=nb_photos;
     }
+    photos_index%=nb_photos
+    photos_actualise()
+}
+
+function photos_actualise()
+{
+    //counter
+    const galerie = document.getElementById("Galerie")
+    galerie.innerText = `Galerie (${photos_index}/${nb_photos})`
+    //photos
     const photos = document.getElementById("photos");
     photos.innerHTML=""
     for (var i=0;i<3;i-=-1)
     {
         var photo = document.createElement("img");
-        photo.src="../img/"+(photos_index+i)%nb_photos+".jpg"
+        photo.src="../img/photos/"+(photos_index+i)%nb_photos+".jpg"
         photo.className="photo_expo"
         photos.appendChild(photo)
     }
 }
+photos_actualise()
