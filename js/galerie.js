0ab89b6102c79f008b69b3c652b5b9fc65f9224c
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2023-11-12 18:04:10 +0100

    restore galerie

--- js/galerie.js
+++ js/galerie.js
@@ -1,17 +1,18 @@
-const nb_photos = 18;
+const nb_photos = 9;
 var photos_index = 0;
 var all_photos = []
+var directory = "photos_vertical"
 
 function next_photos()
 {
-    photos_index-=-3;
+    photos_index-=-2;
     photos_index%=nb_photos
     photos_actualise()
 }
 
 function previous_photos()
 {
-    photos_index+=-3;
+    photos_index+=-2;
     if (photos_index<0)
     {
         photos_index+=nb_photos;
@@ -30,10 +31,10 @@ async function photos_actualise(first=false)
     photos.innerHTML=""
     if (first)
     {
-        for (var i=0;i<3;i-=-1)
+        for (var i=0;i<2;i-=-1)
         {
             var photo = document.createElement("img");
-            photo.src="../img/photos/"+(photos_index+i)%nb_photos+".webp"
+            photo.src= `../img/${directory}/${(photos_index+i)%nb_photos}.webp`
             photo.className="photo_expo"
             photos.appendChild(photo)
             all_photos[(photos_index+i)%nb_photos]=photo
@@ -42,7 +43,6 @@ async function photos_actualise(first=false)
     else{
         photos.appendChild(all_photos[photos_index])
         photos.appendChild(all_photos[(photos_index+1)%nb_photos])
-        photos.appendChild(all_photos[(photos_index+2)%nb_photos])
     }
 }
 
@@ -53,7 +53,7 @@ async function preload()
     for (var i=0;i<nb_photos;i++)
     {
         photo = document.createElement("img");
-        photo.src=`../img/photos/${i}.webp`
+        photo.src=`../img/${directory}/${i}.webp`
         photo.className="photo_expo"
         all_photos[(photos_index+i)%nb_photos]=photo
     }
